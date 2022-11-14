532f9c595426e650304b70b37e2743e744c0fa69
Lima9855 <thelimaks@gmail.com>
2022-11-14 11:13:55 +0100

    fixing connection with cassandra

--- src/main/java/sf/lima/bookapp/BookAppApplication.java
+++ src/main/java/sf/lima/bookapp/BookAppApplication.java
@@ -3,7 +3,11 @@ package sf.lima.bookapp;
 import connection.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
@@ -11,6 +15,12 @@ public class BookAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookAppApplication.class, args);
+    }
+
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
     }
 
 }
