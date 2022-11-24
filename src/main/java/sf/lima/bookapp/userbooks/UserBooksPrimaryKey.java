a89fa1ae9e30e6ee690dbd9e9d87897b644cf487
Lima9855 <thelimaks@gmail.com>
2022-11-24 01:20:18 +0100

    adding user book controller

--- src/main/java/sf/lima/bookapp/userbooks/UserBooksPrimaryKey.java
+++ src/main/java/sf/lima/bookapp/userbooks/UserBooksPrimaryKey.java
@@ -1,6 +1,5 @@
 package sf.lima.bookapp.userbooks;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
--- src/main/java/sf/lima/bookapp/userbooks/UserBooksRepository.java
+++ src/main/java/sf/lima/bookapp/userbooks/UserBooksRepository.java
@@ -1,7 +1,9 @@
 package sf.lima.bookapp.userbooks;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserBooksRepository extends CassandraRepository<UserBooks, UserBooksPrimaryKey> {
     
 }
